bd52f41056acf5e1ff3d4a8da5ec20eabd8024fe
niinkoi <nkhoinguyen26@gmail.com>
2021-07-06 15:52:42 +0700

    UPDATE AUTHENTICATION WITH USERNAME OR EMAIL

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,12 +24,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,
+         authentication_keys: [:login]
+         # validatableauthentication_keys: [:login],
+         # reset_password_keys: [:login]
+
   validates_uniqueness_of :email, :username
   validates_presence_of :email, :username, :first_name
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, messages: 'must be a valid format of email'
 
-  before_save :ensure_proper_name
+  attr_writer :login
 
   has_many :bonds
   has_many :categories
@@ -53,6 +57,49 @@ class User < ApplicationRecord
            -> { Bond.following },
            through: :inward_bonds,
            source: :user
+
+  before_save :ensure_proper_name
+
+  def to_param
+    username
+  end
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_authenticatable(login)
+    where('username = :value OR email = :value', value: login).first
+  end
+
+  def self.find_for_database_authentication(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    find_authenticatable(login)
+  end
+
+  def self.send_reset_password_instructions(conditions)
+    recoverable = find_recoverable_or_init_with_errors(conditions)
+
+    if recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+
+    recoverable
+  end
+
+  def self.find_recoverable_or_init_with_errors(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    recoverable = find_authenticatable(login)
+
+    unless recoverable
+      recoverable = new(login: login)
+      recoverable.errors.add(:login, login.present? ? :not_found : :blank)
+    end
+
+    recoverable
+  end
 
   private
 
